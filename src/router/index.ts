0150db22b055390871942a70fbf4d90232df4cd0
zhongwy <merpokcasterl@yandex.ru>
2025-05-13 04:52:17 +0300

    404 not found added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FaqView from '../views/FaqView.vue'
 import DocsView from '../views/DocsView.vue'
 import HomePageView from '../views/HomePageView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'homePage',
+    component: HomePageView,
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: HomeView,
+  },
+  {
+    path: '/faq',
+    name: 'FAQ',
+    component: FaqView
+  },
+  {
+    path: '/docs',
+    name: 'docs',
+    component: DocsView,
+  },
+  // Добавляем обработку 404 в самом конце
+  {
+    path: '/:pathMatch(.*)*', // Ловит все пути, которые не совпали с вышеперечисленными
+    name: 'not-found',
+    component: NotFoundView,
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'homePage',
-      component: HomePageView,
-    },
-    {
-      path: '/blog',
-      name: 'blog',
-      component: HomeView,
-    },
-    { path: '/faq', name: 'FAQ', component: FaqView },
-    {
-      path: '/docs',
-      name: 'docs',
-      component: DocsView,
-    },
-  ],
+  routes,
 })
 
 export default router
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Страница не найдена</h1>
+    <p>Извините, запрашиваемая страница не существует.</p>
+    <router-link to="/">Вернуться на главную</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NotFoundView',
+});
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
